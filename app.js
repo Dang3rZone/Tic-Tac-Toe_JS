e506387bb3986e99f65948b1cbade41555821fce
Andres Rosales <andresrosalespinzon@gmail.com>
2021-02-01 19:59:44 +0100

    all winning combos,  winning msg, check winner

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 'use strict';
 const xClass = 'x';
 const circleClass = 'circle';
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 const cellElements = document.querySelectorAll('[data-cell');
 const board = document.getElementById('board');
+const winningMessageTextElement = document.querySelector(
+  '[data-winning-message-text]'
+);
 let circleTurn;
 
 startGame();
@@ -19,12 +32,24 @@ function handleClick(e) {
   const cell = e.target;
   const currentClass = circleTurn ? circleClass : xClass;
   placeMark(cell, currentClass);
+  if (checkWin(currentClass)) {
+    // console.log('winner winner chicken dinner');
+    endGame(false);
+  }
   //placeMark
   //CHECK FOR WIN
   //CHEACK FOR DRAW
   // SWITCH TURNS
   swapTurns();
   setBoardHoverClass();
+}
+function endGame(draw) {
+  if (draw) {
+  } else {
+    winningMessageTextElement.innerText = `${
+      circleTurn ? "O's" : "X's"
+    } Wins! `;
+  }
 }
 
 function placeMark(cell, currentClass) {
@@ -43,4 +68,12 @@ function setBoardHoverClass() {
   } else {
     board.classList.add(xClass);
   }
+}
+
+function checkWin(currentClass) {
+  return winningCombos.some((combos) => {
+    return combos.every((index) => {
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
 }
