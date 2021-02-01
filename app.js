4e8b757e4a4e8619a34a1fba9c2dbefedbf7f64d
Andres Rosales <andresrosalespinzon@gmail.com>
2021-02-01 19:34:57 +0100

    queryall cells, added fucn to print both circle and x and swap between them

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 'use strict';
+const xClass = 'x';
+const circleClass = 'circle';
+const cellElements = document.querySelectorAll('[data-cell');
+let circleTurn;
+
+cellElements.forEach((cell) => {
+  cell.addEventListener('click', handleClick, { once: true });
+});
+
+function handleClick(e) {
+  const cell = e.target;
+  const currentClass = circleTurn ? circleClass : xClass;
+  placeMark(cell, currentClass);
+  //placeMark
+  //CHECK FOR WIN
+  //CHEACK FOR DRAW
+  // SWITCH TURNS
+  swapTurns();
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+  circleTurn = !circleTurn;
+}
